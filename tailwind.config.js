00337867961b17c69464cbe98c4f75b1425b7be8
Joel <j_90@live.co.uk>
2021-05-25 22:42:26 +0100

    completes testimonial section

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ module.exports = {
         notoSansJP: ["'Noto Sans JP'", "helvetica", "sans-serif"],
       },
       backgroundImage: (theme) => ({
-        "hero-image": "url('/img/hero-image.jpg')",
+        "hero-image": "url('../dist/img/hero-image.jpg')",
       }),
     },
   },
